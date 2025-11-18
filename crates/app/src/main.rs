5305d98588243707a62ea4d0f5c2f5ecfb99205a
Paul Delafosse <paul.delafosse@protonmail.com>
2025-11-18 15:36:42 +0100

    refactor: remove dx component default style

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -35,7 +35,6 @@ enum Route {
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const COMPONENT_CSS: Asset = asset!("/assets/dx-components-theme.css");
 
 fn main() {
     dioxus::launch(App);
@@ -117,7 +116,6 @@ fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        document::Link { rel: "stylesheet", href: COMPONENT_CSS }
         document::Script {
             type: "module",
             src: asset!("/assets/howler.min.js", JsAssetOptions::new().with_minify(true)),
